dcef00199b577520ff9703c3aec8f20e140a39a8
sebabuel <sebastian.buelter@study.hs-duesseldorf.de>
2022-01-17 18:09:01 +0100

    Moved API debug buttons in separate Svelte component

---
+++ frontend/src/ApiDebug.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+
+    let message:string = ""
+  
+    async function handleRequest() {
+      const response = await fetch('/api/ping');
+      message = (await response.json()).message;
+    }
+  
+    async function getResults() {
+      const response = await fetch('/api/get_results/MMV_POSITIVE');
+      console.log('response: ', response);
+      message = (await response.json());
+  
+    }
+  
+    async function startMs2query() {
+      void (await fetch('/api/run_ms2query'));
+    }
+  </script>
+  
+  
+
+  <div>
+  
+        <button on:click="{() => handleRequest()}">Ping</button>
+        <p>
+          {message}
+        </p>
+      
+        <button on:click="{() => startMs2query()}">Start ms2query test</button>
+        
+        <button on:click="{() => getResults()}">get results</button>
+        <p>
+          {message}
+        </p>
+  
+  </div>
+  
+  <style lang="scss">
+  </style>
+  
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,38 +1,9 @@
 <script lang="ts">
 
-  let message:string = ""
-
-  async function handleRequest() {
-    const response = await fetch('/api/ping');
-    message = (await response.json()).message;
-  }
-
-  async function getResults() {
-    const response = await fetch('/api/get_results/MMV_POSITIVE');
-    console.log('response: ', response);
-    message = (await response.json());
-
-  }
-
-  async function startMs2query() {
-    void (await fetch('/api/run_ms2query'));
-  }
 </script>
 
 <main>
-  <button on:click="{() => handleRequest()}">Ping</button>
-  <p>
-    {message}
-  </p>
-
-  <button on:click="{() => startMs2query()}">Start ms2query test</button>
-  
-  <button on:click="{() => getResults()}">get results</button>
-  <p>
-    {message}
-  </p>
-
-
+  Content
 </main>
 
 <style lang="scss">
@@ -42,8 +13,6 @@
   }
 
   main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
+    background-color: #f5f5f5;
   }
 </style>
