b7484dfd3bdec9ddce7e6b3ae5a20ab3f77e5281
sebabuel <sebastian.buelter@study.hs-duesseldorf.de>
2022-02-09 21:27:09 +0100

    Added Button.svelte

---
+++ frontend/src/components/Button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    export let id: string;
+</script>
+
+<button id="{id}" on:click="{() => dispatch('click')}">
+    <slot />
+</button>
+
+<style lang="scss">
+    button {
+        padding: 0.5rem;
+        cursor: pointer;
+        background-color: #003f63;
+        border: none;
+        border-radius: 0.2rem;
+        color: #d9d9d9;
+
+        &:hover {
+            background-color: lighten(#003f63, 10%);
+        }
+    }
+</style>
