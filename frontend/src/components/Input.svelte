68e1cf6c84f616dd40561a8e224a93ac28bdb69c
sebabuel <sebastian.buelter@study.hs-duesseldorf.de>
2022-02-09 18:56:10 +0100

    Added Input.svelte

---
+++ frontend/src/components/Input.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let label: string | undefined = undefined;
+    export let value: string = "";
+</script>
+
+<div class="search-input">
+    {#if label}
+        <label for="search-input">{label}:</label>
+    {/if}
+    <input bind:value type="text" id="search-input" name="search-input" />
+</div>
+
+<style lang="scss">
+    .search-input {
+    }
+</style>
