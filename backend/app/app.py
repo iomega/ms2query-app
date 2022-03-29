44553bb402ce5272c5727ed5d93d0a2f952aac6b
sebabuel <sebastian.buelter@study.hs-duesseldorf.de>
2022-03-29 21:22:52 +0200

    Add ms2query result to database conversion method

--- backend/app/app.py
+++ backend/app/app.py
@@ -21,16 +21,6 @@ db = mongodb_client.db
 def pong():
     return "pong"
 
-@app.route('/create_ms2query_result', methods=['GET', 'POST'])
-def createMs2QueryResult():
-    data = request.get_json(force=True)
-    db.ms2query_results.insert_one(data)
-
-    return jsonify(
-        status=True,
-        message='Ms2query result saved successfully!'
-    ), 201
-
 @app.route('/get_ms2query_results', methods=['GET'])
 def getMs2QueryResults():
     _ms2query_results = db.ms2query_results.find()
@@ -42,7 +32,7 @@ def getMs2QueryResults():
             '_id': str(result['_id']),
             'timestamp': result['timestamp'],
             'results': result['results'],
-            # 'filename': result['filename']
+            'filename': result['filename']
         }
         data.append(item)
 
@@ -53,39 +43,23 @@ def getMs2QueryResults():
 
 @app.route('/run_ms2query', methods=['GET', 'POST'])
 def runMs2Query():
-    # data = request.get_json(force=True)
-
-    # run ms2query 
-    # run_ms2query()
-
-    filename = os.listdir('./ms2_spectra/results')[0]
-
-    json_array = []
-    with open(r'./ms2_spectra/results/'+filename, encoding='utf-8') as csvf: 
-        csv_reader = csv.DictReader(csvf) 
-
-        for row in csv_reader: 
-            json_array.append(row)
-    
-    db.ms2query_results.insert_one({
-        "timestamp": datetime.datetime.now(),
-        "filename": filename,
-        "results": json_array
-    })
-
-    # remove file & results folder
-    # os.remove(r'./ms2_spectra/results/MMV_POSITIVE.csv')
-    # os.rmdir(r'./ms2_spectra/results')
-
+    run_ms2query()
     return jsonify(
         status=True,
         message='Ms2query result saved successfully!'
+    ), 201
+    
+@app.route('/ms2query_to_database', methods=['GET', 'POST'])
+def ms2queryToDatabase():
+    ms2query_to_database()
+    return jsonify(
+        status=True,
+        message='Results folder successfully removed!'
     ), 201
 
 @app.route('/delete_ms2query_result/<id>', methods=['GET', 'POST'])
 def deleteMs2QueryResult(id):
     db.ms2query_results.delete_one({"_id": ObjectId(id)})
-
     return jsonify(
         status=True,
         message='Ms2query result deleted!'
@@ -102,7 +76,7 @@ def findMs2QueryResult(id):
             'id': str(result['_id']),
             'timestamp': result['timestamp'],
             'results': result['results'],
-            # 'filename': result['filename']
+            'filename': result['filename']
         }
         data.append(item)
 
@@ -110,11 +84,6 @@ def findMs2QueryResult(id):
         status=True,
         data=data
     )
-
-# @app.route('/get_results/<filename>')
-# def results(filename):
-#     file = utils.csv2json(r'./ms2_spectra/results/library_search/'+filename+'.csv', r'./ms2_spectra/results/library_search/'+filename+'.json')
-#     return file
 
 # if __name__ == '__main__':
 #     app.run(host="0.0.0.0", port=5000)
@@ -137,3 +106,35 @@ def run_ms2query():
     run_complete_folder(ms2library, ms2_spectra_directory, folder_to_store_results)
 
     return "ms2query DONE"
+
+def ms2query_to_database():
+    filename = os.listdir('./ms2_spectra/results/analog_search')[0]
+
+    json_array_analog_search = []
+    with open(r'./ms2_spectra/results/analog_search/'+filename, encoding='utf-8') as csvf: 
+        csv_reader = csv.DictReader(csvf) 
+
+        for row in csv_reader: 
+            json_array_analog_search.append(row)
+
+    json_array_library_search = []
+    with open(r'./ms2_spectra/results/library_search/'+filename, encoding='utf-8') as csvf: 
+        csv_reader = csv.DictReader(csvf) 
+
+        for row in csv_reader: 
+            json_array_library_search.append(row)
+    
+    db.ms2query_results.insert_one({
+        "timestamp": datetime.datetime.now(),
+        "filename": filename,
+        "results": {
+            "analog_search": json_array_analog_search,
+            "library_search": json_array_library_search,
+        }
+    })
+
+    os.remove(r'./ms2_spectra/results/analog_search/'+filename)
+    os.remove(r'./ms2_spectra/results/library_search/'+filename)
+    os.rmdir(r'./ms2_spectra/results/analog_search')
+    os.rmdir(r'./ms2_spectra/results/library_search')
+    os.rmdir(r'./ms2_spectra/results')
