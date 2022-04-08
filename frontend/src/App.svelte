4374c042ecd0bf2232ebe6af5f10793ea071f8ec
sebabuel <sebastian.buelter@study.hs-duesseldorf.de>
2022-04-08 17:14:45 +0200

    Remove unused scss variables

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -124,11 +124,4 @@
             }
         }
     }
-
-    /* Color Theme Swatches in Hex */
-    $LOFT-REDUX-LMC-1-hex: #353d40;
-    $LOFT-REDUX-LMC-2-hex: #d9d9d9;
-    $LOFT-REDUX-LMC-3-hex: #a1a5a6;
-    $LOFT-REDUX-LMC-4-hex: #f2b138;
-    $LOFT-REDUX-LMC-5-hex: #003f63;
 </style>
